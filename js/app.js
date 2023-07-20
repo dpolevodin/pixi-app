8a71f900f6da77273995d45e1d908a4d2c9722ee
Полеводин Дмитрий Игоревич (4094029) <dipolevodin@vtb.ru>
2023-07-20 21:20:45 +0600

    feat: added textures preloading

--- js/app.js
+++ js/app.js
@@ -3,17 +3,29 @@ let player;
 let keys = {};
 let keysDiv;
 
-window.onload = function() {
+// TODO: добавить скрытый инпут с версией приложения
+window.onload = async function() {
     app = new PIXI.Application({
         width: window.innerWidth,
         height: window.innerHeight,
         backgroundColor: 0xAAAAAA,
-    });
+    }); 
 
     document.body.appendChild(app.view);
 
+    /** Предварительная загрузка текстур */
+    const textures = await PIXI.Assets.load([
+        { alias: 'player', src: 'images/player4.png'},
+        { alias: 'tree1', src: 'images/tree1.png'},
+        { alias: 'tree2', src: 'images/tree2.png'},
+        { alias: 'tree3', src: 'images/tree3.png'},
+        { alias: 'tree4', src: 'images/tree4.png'},
+        { alias: 'tree5', src: 'images/tree5.png'},
+    ], showProgress);
+    player = PIXI.Sprite.from(textures['player']);
+
     /** Объект игрока */
-    player = PIXI.Sprite.from('images/player4.png');
+    // player = PIXI.Sprite.from('images/player4.png');
     /** Привязка оси к центру, по умолчанию (левый верхний угол) */
     player.anchor.set(0.5)
     player.x = app.view.width / 2;
@@ -23,7 +35,7 @@ window.onload = function() {
     app.stage.addChild(player);
 
     /** Интерактивность мыши */
-    // app.stage.interactive = true;
+    app.stage.interactive = true;
     // app.stage.on("pointermove", movePlayer);
 
     window.addEventListener('keydown', keyDown);
@@ -35,6 +47,10 @@ window.onload = function() {
     // TODO: delete later
     keysDiv = document.querySelector("#keys");
 };
+
+function showProgress(progress) {
+    console.log(`${Math.floor(progress * 100)}% loading`)
+}
 
 function keyDown(e) {
     console.log(e.key)
